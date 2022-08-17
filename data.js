7462cc95782005fa84a757df1aa006681a242cd2
Sourabh1Bhardwaj <sourabh.bhardwaj202@gmail.com>
2022-08-17 19:07:31 +0530

    Create a File

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name:'Sourabh',email:'e@gmail.com'},
+    {name:'ourabh',email:'e@gmail.com'},
+    {name:'urabh',email:'e@gmail.com'}
+];
+module.exports=data;
---
+++ index.js
@@ -0,0 +1,30 @@
+// const fs = require('fs');
+// const input = process.argv;
+
+// if(input[2]=='add'){
+//     fs.writeFileSync(input[3],input[4]);
+// }
+// else if(input[2]=='remove'){
+//     fs.unlinkSync(input[3]);
+// }
+// else{
+//     console.log('Invalid input');
+// }
+
+//Create multiple file in File folder
+// const fs = require('fs');
+// const path = require('path');
+// const dirPath = path.join(__dirname,'Files');
+
+// // for(var i=0;i<5;i++){
+// //     fs.writeFileSync(dirname+`/hello${i}.txt`,"Hii to all");
+// // }
+
+// fs.readdir(dirPath,(err,files)=>{
+//     files.forEach((item)=>{
+//         console.log(item);
+//     });
+// });
+
+//CRUD Operation
+
