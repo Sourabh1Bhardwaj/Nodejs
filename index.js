059698a9342f46425f3fed0e46dc2de2c000d6ef
Sourabh1Bhardwaj <sourabh.bhardwaj202@gmail.com>
2022-08-17 19:37:14 +0530

    Perform All CRUD Operation

--- index.js
+++ index.js
@@ -27,4 +27,21 @@
 // });
 
 //CRUD Operation
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname,'CRUD');
+const filePath = `${dirPath}/apple.txt`;
 
+// fs.writeFileSync(filePath,'This is a simple text file');
+// fs.readFile(filePath,'utf-8',(err,item)=>{
+//     console.log(item);
+// });
+// fs.appendFile(filePath,' and file name is apple.txt',(err)=>{
+//     if(!err) console.log("File is updated");
+// });
+
+// fs.rename(filePath,`${dirPath}/fruit.txt`,(err)=>{
+//     if(!err) console.log('File name is updated');
+// });
+
+fs.unlinkSync(`${dirPath}/fruit.txt`);
